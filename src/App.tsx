4897206038944c06b1eeddbd7370d0edd5e3be08
anandhu2311c <anandhupersonal08@gmail.com>
2025-08-11 23:28:07 +0530

    added new cursor animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Aurora from './components/Aurora';
+import SplashCursor from './components/SplashCursor'; 
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Certificates from './pages/Certificates';
@@ -12,6 +13,10 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen bg-[#0a0a0a]">
+        {}
+        <SplashCursor />
+        
+        {/* Aurora Background */}
         <div className="fixed inset-0 z-0">
           <Aurora
             colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
@@ -20,27 +25,29 @@ function App() {
             speed={0.5}
           />
         </div>
+        
+        {/* Main Content */}
         <div className="relative z-10">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <section id="home" className="section">
-                <Home />
-              </section>
-              <section id="projects" className="section">
-                <Projects />
-              </section>
-              <section id="certificates" className="section">
-                <Certificates />
-              </section>
-              <section id="contact" className="section">
-                <Contact />
-              </section>
-            </>
-          } />
-          <Route path="/project/:id" element={<ProjectDetail />} />
-        </Routes>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={
+              <>
+                <section id="home" className="section">
+                  <Home />
+                </section>
+                <section id="projects" className="section">
+                  <Projects />
+                </section>
+                <section id="certificates" className="section">
+                  <Certificates />
+                </section>
+                <section id="contact" className="section">
+                  <Contact />
+                </section>
+              </>
+            } />
+            <Route path="/project/:id" element={<ProjectDetail />} />
+          </Routes>
         </div>
       </div>
     </Router>
